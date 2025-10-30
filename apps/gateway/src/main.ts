820d03752ff004b8343ee22ef9a95b200b5ada30
CarlaOnate <carlaonategardella@gmail.com>
2025-10-30 13:13:23 +0100

    Feat: Output Validation - Adds response transformation to /gateway

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { GatewayModule } from './gateway.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { TransformGatewayInterceptor } from '../../../common/interceptors/transform-gateway-interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
   const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new TransformGatewayInterceptor());
   await app.listen(configService.get<number>('PORT') ?? 3000);
 }
 bootstrap();
--- common/interceptors/transform-user-interceptor.ts
+++ common/interceptors/transform-user-interceptor.ts
@@ -1,4 +1,3 @@
-// transform.interceptor.ts
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
